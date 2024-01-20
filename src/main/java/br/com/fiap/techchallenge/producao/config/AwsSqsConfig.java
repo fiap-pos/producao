85ab91bb1d9e67fb02dea136748adde584116a10
Vanderson Nunes <vann.nunes@gmail.com>
2024-01-20 15:22:40 -0300

    SQS Configuration done!

--- src/main/java/br/com/fiap/techchallenge/producao/config/AwsSqsConfig.java
+++ src/main/java/br/com/fiap/techchallenge/producao/config/AwsSqsConfig.java
@@ -1,7 +1,7 @@
 package br.com.fiap.techchallenge.producao.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.awspring.cloud.sqs.config.SqsListenerConfigurer;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +9,19 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
 public class AwsSqsConfig {
-    @Bean
-    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient, ObjectMapper objectMapper){
-        objectMapper.registerModule(new JavaTimeModule());
 
+    @Bean
+    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient, ObjectMapper objectMapper) {
         return SqsTemplate.builder().configureDefaultConverter(converter -> {
                     converter.setObjectMapper(objectMapper);
                 }).sqsAsyncClient(sqsAsyncClient)
                 .build();
+    }
+
+    @Bean
+    SqsListenerConfigurer configurer(ObjectMapper objectMapper) {
+        return registrar -> {
+            registrar.setObjectMapper(objectMapper);
+        };
     }
 }
